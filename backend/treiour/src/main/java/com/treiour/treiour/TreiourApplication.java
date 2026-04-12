dd5560a2db1bc07655782749ae63d66611a6c2f7
Arthur Lima Ondichiatti <arthur.ondichiatti03@gmail.com>
2026-04-12 18:09:44 -0300

    tela seu treino + spring boot

---
+++ backend/treiour/src/main/java/com/treiour/treiour/TreiourApplication.java
@@ -0,0 +1,13 @@
+package com.treiour.treiour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreiourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreiourApplication.class, args);
+	}
+
+}
---
+++ backend/treiour/src/test/java/com/treiour/treiour/TreiourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treiour.treiour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreiourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
