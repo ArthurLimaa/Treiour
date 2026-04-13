372d58cb04b023c12734c507b4db4913d6f051d8
Arthur Lima Ondichiatti <arthur.ondichiatti03@gmail.com>
2026-04-13 07:47:54 -0300

    Criando classes

---
+++ backend/treiour/src/main/java/service/TreinoService.java
@@ -0,0 +1,11 @@
+package service;
+
+public class TreinoService {
+
+    public List<Treino> listarTreinos() {    
+        // Implement logic to retrieve and return list of trainings
+        return new ArrayList<>();
+    }
+
+    
+}
